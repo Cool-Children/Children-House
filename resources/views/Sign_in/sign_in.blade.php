4768323a6915e054cb061e14d728c31276c80d13
Mishan Madhupa <mishancool2016@gmail.com>
2019-11-13 08:56:40 +0530

    Create home route

--- resources/views/Sign_in/sign_in.blade.php
+++ resources/views/Sign_in/sign_in.blade.php
@@ -25,6 +25,66 @@
 
 </head>
 <body>
+
+        <div class="bg-top navbar-light">
+                <div class="container">
+                    <div class="row no-gutters d-flex align-items-center align-items-stretch">
+                        <div class="col-md-4 d-flex align-items-center py-4">
+                            <a class="navbar-brand" href="index.html">Children's <span>House</span></a>
+                        </div>
+                        <div class="col-lg-8 d-block">
+                            <div class="row d-flex">
+                                <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+                                    <div class="icon d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+                                    <div class="text">
+                                        <span>Email</span>
+                                        <span>children'shouse@email.com</span>
+                                    </div>
+                                </div>
+                                <div class="col-md d-flex topper align-items-center align-items-stretch py-md-4">
+                                    <div class="icon d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+                                    <div class="text">
+                                        <span>Call</span>
+                                        <span>Call Us: +94 1234567</span>
+                                    </div>
+                                </div>
+                                <div class="col-md topper d-flex align-items-center justify-content-end">
+                                    <p class="mb-0">
+                                        <a href="{{ URL::to('/sign_in') }}" class="btn py-2 px-3 btn-primary d-flex align-items-center justify-content-center">
+                                            <span>Sign In</span>
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+            </div>
+              <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+                <div class="container d-flex align-items-center px-4">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="oi oi-menu"></span> Menu
+                  </button>
+                  <form action="#" class="searchform order-lg-last">
+                  <div class="form-group d-flex">
+                    <input type="text" class="form-control pl-3" placeholder="Search">
+                    <button type="submit" placeholder="" class="form-control search"><span class="ion-ios-search"></span></button>
+                  </div>
+                </form>
+                  <div class="collapse navbar-collapse" id="ftco-nav">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a href="{{ URL::to('/') }}" class="nav-link pl-0">Home</a></li>
+                        <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+                        <li class="nav-item"><a href="courses.html" class="nav-link">Courses</a></li>
+                        <li class="nav-item"><a href="teacher.html" class="nav-link">Staff</a></li>
+                        <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+                      <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            <!-- END nav -->
+
     <section class="ftco-section ftco-consult ftco-no-pt ftco-no-pb" style="background-image: url(images/sign_in1.jpg);" data-stellar-background-ratio="0.5">
     	<div class="overlay"></div>
     	<div class="container">
