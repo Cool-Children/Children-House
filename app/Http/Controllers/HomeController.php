415360bd491efa7b2b5918bbd6d5255d25e05b98
Mishan Madhupa <mishancool2016@gmail.com>
2019-11-25 14:52:15 +0530

    Create user profile to normal users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('Users.Normal_users.normal_users ');
         // return view('/Normal_users/normal_users');
         // return view('/w');
     }
